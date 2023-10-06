490fd8746ee7088d120eb6043883144453436085
NaahilKDJ <naahil34@gmail.com>
2023-10-06 14:56:30 +0200

    HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DashBoardProject } from './components/dashBoardProject/DashBoardProject';
 import { Profile } from './components/profile/profile';
 import './App.css'
+import Home from './route/Home';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Routes>
               <Route path="/Profile" element={<Profile />}></Route>
               <Route path="/DashBoard/:id" element={<DashBoardProject />}></Route>
+              <Route path="/Home" element={<Home/>} />
             </Routes>
           </Router>
         </FirebaseProvider>
---
+++ src/route/Home.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import NavBar from "./NavBar.jsx";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+export default function Home() {
+
+    return(
+        <div>
+            <NavBar></NavBar>
+            <h1>Projekto</h1>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Card sx={{ maxWidth: 360 }}>
+                        <CardActionArea>
+                            <CardMedia component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana"/>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Bienvenue sur Projekto
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Regroupez votre équipe et commencez donc à vous organniser <br />
+                                    Nous vous permettrons de mettre à l'écris votre organnisation et votre planning ce qui vous permettre de toujours garder le cap
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={6}>
+                    <Card sx={{ maxWidth: 360 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana"/>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Déterminez :
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    <ul>
+                                        <li>Vos outils de développement</li>
+                                        <li>Vos Différentes étapes</li>
+                                        <li>Les paliers a atteindre</li>
+                                        <li>Les tâches à accomplir et leur responsables respectif</li>
+                                    </ul>
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid container item justifyContent="center" direction="row" alignItems="center">
+                    <div>
+                        <Typography variant="body2" color="text.secondary">
+                            Votre organnisation concrétise vos idées ! 
+                        </Typography>
+                            <br />
+                        <Button variant="outlined" size="medium">Démarrer un Projet</Button>
+                    </div>
+                        
+                   
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
